890fbcf8618846db3aab31cd835137b2391b874c
cnulenka <sinulenka001@gmail.com>
2021-04-04 03:43:56 +0530

    added homepage html, added new views, urls

--- cnusic/api/views.py
+++ cnusic/api/views.py
@@ -4,6 +4,7 @@ from api.serializers import RoomSerializer, CreateRoomSerializer
 from api.models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 # Create your views here.
 
@@ -77,3 +78,13 @@ class JoinRoom(APIView):
             return Response({'Bad Request': 'Invalid Room Code'}, status=status.HTTP_400_BAD_REQUEST)
 
         return Response({'Bad Request': 'Invalid post data, did not find a code key'}, status=status.HTTP_400_BAD_REQUEST)
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
--- cnusic/frontend/src/components/HomePage.js
+++ cnusic/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
 import Room from "./Room";
+import { Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,22 +12,65 @@ import {
 } from "react-router-dom";
 
 export default class HomePage extends Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      roomCode: null,
+    };
+  }
 
-    render(){
-        return (
-        <Router>
-            <Switch>
-            <Route exact path="/">
-                <p>This is the home page</p>
-            </Route>
-            <Route path="/join" component={RoomJoinPage} />
-            <Route path="/create" component={CreateRoomPage} />
-            <Route path="/room/:roomCode" component={Room} />
-            </Switch>
-        </Router>
-        );
-    }
+  async componentDidMount() {
+    fetch("/api/user-in-room")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({
+          roomCode: data.code,
+        });
+      });
+  }
+
+  renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          <Typography variant="h3" compact="h3">
+            Cnusic
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color="primary" to="/join" component={Link}>
+              Join a Room
+            </Button>
+            <Button color="secondary" to="/create" component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return this.state.roomCode ? (
+                <Redirect to={`/room/${this.state.roomCode}`} />
+              ) : (
+                this.renderHomePage()
+              );
+            }}
+          />
+          <Route path="/join" component={RoomJoinPage} />
+          <Route path="/create" component={CreateRoomPage} />
+          <Route path="/room/:roomCode" component={Room} />
+        </Switch>
+      </Router>
+    );
+  }
 }
